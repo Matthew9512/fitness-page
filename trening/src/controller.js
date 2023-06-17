daf0cf83381d68a7f8d14e9d24ebf49516e45964
Matthew9512 <mateuszmaciak95@gmail.com>
2023-06-17 13:30:35 +0200

    prod

--- trening/src/controller.js
+++ trening/src/controller.js
@@ -4,24 +4,24 @@ const exerciseArticle = document.querySelector('.exercise-article');
 const bookmarkDrop = document.querySelector('.bookmark-drop');
 
 // fetch body parts name
-// if (navigator.onLine) model.getBodyParts();
+if (navigator.onLine) model.getBodyParts();
 
 // scroll into exerciseArticle
 export const scroll = function () {
-  const exerciseArticle = document.querySelector('.exercise-article');
-  exerciseArticle.scrollIntoView();
+   const exerciseArticle = document.querySelector('.exercise-article');
+   exerciseArticle.scrollIntoView();
 };
 
 asideBodyModel.addEventListener('click', model.getExercises);
 exerciseArticle.addEventListener('click', (e) => {
-  const click = e.target;
-  if (!click.closest('.exercise-item')) return;
-  else if (click.classList.contains('bookmark')) model.bookmark(click);
-  else model.getExerciseByID(click);
+   const click = e.target;
+   if (!click.closest('.exercise-item')) return;
+   else if (click.classList.contains('bookmark')) model.bookmark(click);
+   else model.getExerciseByID(click);
 });
 
 // fetch display bookmark item after click
 bookmarkDrop.addEventListener('click', (e) => {
-  const click = e.target;
-  model.getExerciseByID(click);
+   const click = e.target;
+   model.getExerciseByID(click);
 });
